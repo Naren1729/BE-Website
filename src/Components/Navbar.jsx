1d0c4c302eb7d5938fb01140d8513b18105c3a01
Naren sri sai <narensai29@gmail.com>
2024-11-07 10:41:36 +0530

    added logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,23 +2,27 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import logo from "./logo1.png";
+import logo from "./logo3.png";
 import "./Navbar.css";
 
 function NavBar() {
   return (
     <>
       <Navbar expand="lg" data-bs-theme="light" sticky="top" className="navBar">
-        <Container>
-          <Navbar.Brand href="#home" className="custom-font-size">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Logo"
-            />
-            Bhagiradha Enterprises
+        <Container className="navbar-container">
+          <Navbar.Brand href="/" className="custom-font-size">
+            <div className="logo-heading">
+              <img
+                src={logo}
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+                alt="Logo"
+              />
+              {/* <div className="Heading1"> */}
+              <b>Bhagiradha Enterprises</b>
+              {/* </div> */}
+            </div>
           </Navbar.Brand>
           {/* Toggle button for mobile */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
